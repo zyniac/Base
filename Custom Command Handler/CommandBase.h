0b50d3027de4feeb9a077b964c0971c7b6bcbff2
3l4ckL1ght <moritz123456782@gmail.com>
2019-08-12 15:23:53 +0200

    Moved Stuff into CommandBase

---
+++ Custom Command Handler/CommandBase.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "CommandHandler.h"
+#include <iostream>
+
+#define EXIT_STRING "exit"
+#define SUBCOMMAND_NF "Subcommand not found"
+#define COMMAND_NF "Command not found"
+#define ARGUMENT_INDICATOR "-"
+
+void RunCmdBase(CommandHandler hCmd, int argc, char** argv) {
+	Command* cmd;
+
+	std::string input;
+
+	if (argc == 1) {
+		do
+		{
+			input = "";
+			char c;
+			while ((c = std::cin.get()) != '\n') {
+				input += c;
+			}
+			std::vector<std::string> args = _split(input, " ");
+			cmd = hCmd.getCommand(args[0].c_str());
+			args.erase(args.begin());
+			if (cmd != nullptr) {
+				cmd->call(args);
+			}
+			else if (input != EXIT_STRING) {
+				std::cout << COMMAND_NF << std::endl;
+			}
+		} while (input != EXIT_STRING);
+	}
+	else if (argc > 1) {
+		std::vector<std::string> args(argv + 1, argv + argc);
+
+		Command* cmd = hCmd.getCommand(args[0].c_str());
+		if (cmd != nullptr) {
+			args.erase(args.begin(), args.begin() + 1);
+			cmd->call(args);
+		}
+		else {
+			std::cout << SUBCOMMAND_NF << std::endl;
+		}
+	}
+}
--- Custom Command Handler/main.cpp
+++ Custom Command Handler/main.cpp
@@ -1,53 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "CommandLib.h"
 #include "CommandHandler.h"
 #include "TestCommand.h"
-
-#define EXIT_STRING "exit"
-#define SUBCOMMAND_NF "Subcommand not found"
-#define COMMAND_NF "Command not found"
-#define ARGUMENT_INDICATOR "-"
+#include "CommandBase.h"
 
 int main(int argc, char** argv) {
 	CommandHandler hCmd;
 	hCmd.registerCommand(new TestCommand);
-
-	Command* cmd;
-
-	std::string input;
-
-	if (argc == 1) {
-		do
-		{
-			input = "";
-			char c;
-			while ((c = std::cin.get()) != '\n') {
-				input += c;
-			}
-			std::vector<std::string> args = _split(input, " ");
-			cmd = hCmd.getCommand(args[0].c_str());
-			args.erase(args.begin());
-			if (cmd != nullptr) {
-				cmd->call(args);
-			}
-			else if (input != EXIT_STRING) {
-				std::cout << COMMAND_NF << std::endl;
-			}
-		} while (input != EXIT_STRING);
-	}
-	else if (argc > 1) {
-		std::vector<std::string> args(argv + 1, argv + argc);
-
-		Command* cmd = hCmd.getCommand(args[0].c_str());
-		if (cmd != nullptr) {
-			args.erase(args.begin(), args.begin() + 1);
-			cmd->call(args);
-		}
-		else {
-			std::cout << SUBCOMMAND_NF << std::endl;
-		}
-	}
+	RunCmdBase(hCmd, argc, argv);
 	return 0;
 }
